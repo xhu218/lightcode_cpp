771e30df8d8d1093a18ccf4609363ceaf803ae84
135****0218 <xhu218@163.com>
2024-03-06 07:10:28 +0000

    二给数组运行

--- main.cpp
+++ main.cpp
@@ -1,25 +1,82 @@
-#include <iostream>
 #include "queen.hh"
-#include "vector.hh"
-#include <string.h>
+#include <iostream>
+//#include "vector.hh"
+#include <array>
 #include <stdio.h>
+#include <string.h>
 #include <string>
 #include <utility>
+#include <vector>
+
 using namespace std;
+namespace AA {
+int aa;
+}
+using namespace AA;
 
-int main()
-{   
-
-  //fun_test_copy_construct();
-
-  //fun_testpara_const();
-  //_fun_virtualfun();
-  //fun_testmain();
-  //fun_testfan();
-  //fun_testfan1();
-
-  //fun_testtemplateinherit();
-  fun_testtemplateclassinherit();
-  return 0;
+template <typename T> void func(const T& arr) {
+    for (int ii = 0; ii < arr.size(); ii++) {
+        for (int jj = 0; jj < arr[ii].size(); jj++) {
+            cout << arr[ii][jj] << " ";
+        }
+        cout << endl;
+    }
 }
 
+void func1(int (*arr)[5], int n) {
+    for (int ii = 0; ii < n; ii++) {
+        for (int jj = 0; jj < 5; jj++) {
+            cout << arr[ii][jj] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+
+    array<array<int, 5>, 5> arr;
+    for (int ii = 0; ii < arr.size(); ii++) {
+        for (int jj = 0; jj < arr[ii].size(); jj++) {
+            arr[ii][jj] = jj * 10 + ii;
+        }
+    }
+    // func(arr);
+
+    int bb[10][5];
+    for (int ii = 0; ii < 10; ii++) {
+        for (int jj = 0; jj < 5; jj++) {
+            bb[ii][jj] = jj * 10 + ii;
+        }
+    }
+    // func1(bb,10);
+
+    /*
+
+    vector<int> v ={1,2,3};
+    for(auto it=v.begin();it!=v.end();it++)
+    {
+        cout<<*it<<endl;
+    }
+
+    for(const int & i:v)
+    {
+      cout<<i<<endl;
+    }
+
+  */
+    cout << "aa = " << aa << endl;
+    // fun_test_copy_construct();
+
+    // fun_testpara_const();
+    //_fun_virtualfun();
+    // fun_testmain();
+    // fun_testfan();
+    // fun_testfan1();
+
+    // fun_testtemplateinherit();
+    // fun_testtemplateclassinherit();
+
+    // cout<<__FILE__<<"$"<<__DATE__<<"$"<<__TIME__<<"$"<<__LINE__<<endl;
+
+    return 0;
+}
